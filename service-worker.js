c80ad16d8d459336f46bb807c1cbb6ab64473cf0
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-22 16:02:50 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.d548e7fc49ace3a8e3dbb2bdc4f5a76a.js"
 );
 
 self.addEventListener('message', (event) => {
